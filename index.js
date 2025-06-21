34b43eb48b0c79806cd4e2720d09f2c1b2ff2e22
moshaosama <mohamedOSFekry@gmail.com>
2025-06-21 03:56:48 +0300

    modified: updated main server entry file

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import { ListeningPort } from "./Utils/ListenningPort.js";
+import errorHandler from "./middleware/error.middleware.js";
+import createBlogRouter from "./Router/Blogs/CreateBlog.Router.js";
+import getBlogsRouter from "./Router/Blogs/GetBlogs.Router.js";
+const app = express();
+//Middelware
+app.use(cors());
+app.use(express.json());
+dotenv.config({ path: "./.env" });
+
+//Endpoints
+app.use("/create-blog", createBlogRouter);
+app.use("/get-blogs", getBlogsRouter);
+
+app.use(errorHandler);
+
+ListeningPort(app);
