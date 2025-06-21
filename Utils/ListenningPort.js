6e394f9760cec246594e6f3d85299205908aae03
moshaosama <mohamedOSFekry@gmail.com>
2025-06-21 03:56:48 +0300

    added: utility functions and helpers

---
+++ Utils/ListenningPort.js
@@ -0,0 +1,19 @@
+import DB from "../Config/DB.js";
+import redisClient from "../Config/redis.js";
+
+export const ListeningPort = (app) => {
+  app.listen(process.env.PORT || 5000, async () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+
+    DB.connect((err) => {
+      if (!err) {
+        console.log("Connected to DB successfully");
+      } else {
+        console.log("Failed to connect to DB");
+      }
+    });
+
+    await redisClient.connect();
+    console.log("Redis connected successfully");
+  });
+};
