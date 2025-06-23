f089ad1259a7647485c2b88a194fa9569abd3bf4
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:42:28 +0300

    Latest Version

--- Config/DB.js
+++ Config/DB.js
@@ -3,11 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
 
-const DB = mysql.createConnection({
+// const DB = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+
+const DB = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default DB;
