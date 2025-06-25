e07d66ddb1c3d5fc4f5258cea249fcf96032e581
moshaosama <mohamedOSFekry@gmail.com>
2025-06-25 20:25:12 +0300

    Latest Version

--- Router/Blogs/GetBlogByID.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { GetBlogByID } from "../../Controller/Blogs/getBlogById";
-
-const GetBlogByIdRouter = express.Router();
-
-GetBlogByIdRouter.route("/:blog_id").get(GetBlogByID);
-
-export default GetBlogByIdRouter;
